a02cb7032484ba4a8f70c5cc485d376d480da635
af950833 <af950833@hanmail.net>
2026-07-06 09:36:48 +0900

    Add files via upload

--- custom_components/smartthinq_sensors/const.py
+++ custom_components/smartthinq_sensors/const.py
@@ -1,6 +1,6 @@
 """Constants for LGE ThinQ custom component."""
 
-__version__ = "0.42.2.24"
+__version__ = "0.42.2.25"
 PROJECT_URL = "https://github.com/af950833/ha-smartthinq-sensors/"
 ISSUE_URL = f"{PROJECT_URL}issues"
 
